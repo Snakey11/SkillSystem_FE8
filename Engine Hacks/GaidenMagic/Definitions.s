4b824f05b2f67e9fe62c96d6ed98c8ed35beb327
Snakey11 <snakey1loa@gmail.com>
2020-03-26 14:22:33 -0400

    Update Definitions.s

--- Engine Hacks/GaidenMagic/Definitions.s
+++ Engine Hacks/GaidenMagic/Definitions.s
@@ -11,6 +11,10 @@ SET_FUNC DrawItemMenuCommand, (0x08016848+1)
 
 SET_FUNC GetWeaponRangeMask, (0x080170D4+1)
 
+SET_FUNC AttackUMEffect, (0x08022B30+1)
+
+SET_FUNC DrawItemRText, (0x08088E60+1)
+
 
 @ Data declarations:
 
@@ -19,6 +23,8 @@ SET_DATA SpellsBuffer, 0x202B6D0
 SET_DATA UsingSpellMenu, 0x0203F082
 
 SET_DATA SelectedSpell, 0x0203F080
+
+SET_DATA SpellTargetSelection, 0x0859D3F8
 
 
 @ Autohooks:
@@ -30,3 +36,5 @@ SET_FUNC NewGetUnitEquippedWeaponSlot, (0x08016B58+1)
 SET_FUNC NewGetUnitRangeMask, (0x080171E8+1)
 
 SET_FUNC SetUpBattleWeaponDataHack, (0x0802A730+1)
+
+SET_FUNC NewMenuRText, (0x08024588+1)
